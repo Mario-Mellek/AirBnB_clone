40dbd39a389651757e250a6c975fdbffdad5004a
Mario-Mellek <mario.samy266@gmail.com>
2024-05-18 18:32:53 +0000

    Edited str method

--- models/base_model.py
+++ models/base_model.py
@@ -21,9 +21,7 @@ class BaseModel:
 
     def __str__(self):
         """Return a string representation of the instance"""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Update the `updated_at` attribute with the current datetime"""
