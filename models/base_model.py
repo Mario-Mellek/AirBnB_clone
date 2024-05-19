fb0410ce89510af362819e8e94832b3208725e18
Mario-Mellek <mario.samy266@gmail.com>
2024-05-19 10:24:52 +0000

    linked storage to base_model and edited docstrings and str method return statment

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 BaseModel module
+
+This module defines the BaseModel class, which serves as the base class for all
+other models in the application.
 """
 
 import uuid
@@ -9,8 +12,12 @@ from datetime import datetime
 
 class BaseModel:
     """
-    BaseModel class: defines all common
-    attributes/methods for other classes
+    BaseModel class: defines all common attributes/methods for other classes.
+
+    Attributes:
+        id (str): Unique identifier for each instance.
+        created_at (datetime): The datetime when the instance was created.
+        updated_at (datetime): The datetime when the instance was last updated.
     """
 
     def __init__(self, *args, **kwargs):
@@ -25,14 +32,18 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the instance"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Update the `updated_at` attribute with the current datetime"""
         self.updated_at = datetime.now()
+        from models import storage
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance"""
