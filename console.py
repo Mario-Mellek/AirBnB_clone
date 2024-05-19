78e699122dc872f7b1059c5a82917e4207df571e
Mario-Mellek <mario.samy266@gmail.com>
2024-05-19 14:33:26 +0000

    created do_create method that Creates a new instance of BaseModel, saves it and prints the id

--- console.py
+++ console.py
@@ -4,11 +4,15 @@
     of the command interpreter.
 """
 import cmd
+import shlex
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter cmd class"""
     prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel}
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -22,6 +26,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Called when an empty line is entered."""
         pass
+
+    def do_create(self, arg):
+        """
+            Creates a new instance of BaseModel,
+            saves it and prints the id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        cls = self.classes[arg]
+        cls_instance = cls()
+        cls_instance.save()
+        print(cls_instance.id)
 
 
 if __name__ == '__main__':
