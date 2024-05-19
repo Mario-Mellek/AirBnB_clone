80611f17bc8834bf256a3f1b38721f9775e4f23c
Mario-Mellek <mario.samy266@gmail.com>
2024-05-19 00:24:31 +0000

    updated base_model with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,18 @@ class BaseModel:
     attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new instance of BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return a string representation of the instance"""
