773675fc74f92c4dd4b764c6194e0e2797a6fded
Mario-Mellek <mario.samy266@gmail.com>
2024-05-19 09:51:01 +0000

    Added file_storage engine methods

---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+FileStorage module for serializing and deserializing JSON files.
+
+This module contains the FileStorage class, which provides methods for
+serializing instances to a JSON file and deserializing JSON files to
+instances.
+"""
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+        - Serializes instances to a JSON file
+        - deserializes JSON file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+            Deserializes the JSON file to __objects
+            (if the JSON file (__file_path) exists).
+        """
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    cls_name = value["__class__"]
+                    if cls_name == "BaseModel":
+                        self.__objects[key] = BaseModel(**value)
