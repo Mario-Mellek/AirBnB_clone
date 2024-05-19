10387354fa281eae119de93a4066bef30eb2b3fc
Mario-Mellek <mario.samy266@gmail.com>
2024-05-19 00:41:28 +0000

    Added additional base_model tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,7 +14,7 @@ class TestBaseModel(unittest.TestCase):
     """Test cases for the BaseModel class"""
 
     def test_initialization(self):
-        """Test initialization of BaseModel instance"""
+        """Test initialization of BaseModel"""
         my_model = BaseModel()
         self.assertIsInstance(my_model, BaseModel)
         self.assertIsNotNone(my_model.id)
@@ -23,14 +23,13 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(my_model.created_at, my_model.updated_at)
 
     def test_str_representation(self):
-        """Test string representation of BaseModel instance"""
+        """Test string representation of instance"""
         my_model = BaseModel()
         expected_str = (
             f"[{my_model.__class__.__name__}] "
             f"({my_model.id}) "
             f"{my_model.__dict__}"
         )
-
         self.assertEqual(str(my_model), expected_str)
 
     def test_save_method(self):
@@ -55,6 +54,53 @@ class TestBaseModel(unittest.TestCase):
             '__class__': 'BaseModel'
         }
         self.assertDictEqual(my_model_dict, expected_dict)
+
+    def test_initialization_with_kwargs(self):
+        """Test initialization of BaseModel instance with kwargs added"""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model_dict = my_model.to_dict()
+        new_model = BaseModel(**my_model_dict)
+        self.assertEqual(new_model.id, my_model.id)
+        self.assertEqual(new_model.created_at, my_model.created_at)
+        self.assertEqual(new_model.updated_at, my_model.updated_at)
+        self.assertEqual(new_model.name, my_model.name)
+        self.assertEqual(new_model.my_number, my_model.my_number)
+
+    def test_datetime_conversion(self):
+        """Test datetime conversion for created_at and updated_at"""
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        new_model = BaseModel(**my_model_dict)
+        self.assertIsInstance(new_model.created_at, datetime)
+        self.assertIsInstance(new_model.updated_at, datetime)
+        self.assertEqual(new_model.created_at, my_model.created_at)
+        self.assertEqual(new_model.updated_at, my_model.updated_at)
+
+    def test_to_dict_output_types(self):
+        """Test to_dict method returns expected types"""
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict['id'], str)
+        self.assertIsInstance(my_model_dict['created_at'], str)
+        self.assertIsInstance(my_model_dict['updated_at'], str)
+        self.assertEqual(my_model_dict['__class__'], 'BaseModel')
+
+    def test_unique_id(self):
+        """Test each BaseModel instance gets a unique id"""
+        model1 = BaseModel()
+        model2 = BaseModel()
+        self.assertNotEqual(model1.id, model2.id)
+
+    def test_save_updates_updated_at(self):
+        """Test save method updates `updated_at` attribute"""
+        my_model = BaseModel()
+        old_updated_at = my_model.updated_at
+        my_model.save()
+        new_updated_at = my_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+        self.assertGreater(new_updated_at, old_updated_at)
 
 
 if __name__ == '__main__':
